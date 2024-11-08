49e72709e9dd83b024d59e331b08b527e3d55daf
Jessica Sturgell <sturgell01@gmail.com>
2024-11-07 22:07:35 -0500

    scenes testing

--- app/main.rb
+++ app/main.rb
@@ -1,85 +1,130 @@
+require 'lib/input.rb'
+
 def tick args
-  args.state.logo_rect ||= { x: 576,
-                             y: 200,
-                             w: 128,
-                             h: 101 }
+  # initialize to title scene
+  args.state.current_scene ||= :title_scene
+  # capture the title scene to verify it doesn't change through the duration of a tick
+  current_scene = args.state.current_scene
 
-  args.outputs.labels  << { x: 640,
-                            y: 600,
-                            text: 'Hello World!',
-                            size_px: 30,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 510,
-                            text: "Documentation is located under the ./docs/docs.txt directory. 150+ samples are located under the ./samples directory.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 480,
-                            text: "If you prefer formatted docs, you can access them locally at http://localhost:9001 or online at http://docs.dragonruby.org.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 400,
-                            text: "The code that powers what you're seeing right now is located at ./mygame/app/main.rb.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 380,
-                            text: "(you can change the code while the app is running and see the updates live)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.sprites << { x: args.state.logo_rect.x,
-                            y: args.state.logo_rect.y,
-                            w: args.state.logo_rect.w,
-                            h: args.state.logo_rect.h,
-                            path: 'dragonruby.png',
-                            angle: Kernel.tick_count }
-
-  args.outputs.labels  << { x: 640,
-                            y: 180,
-                            text: "(use arrow keys to move the logo around)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 80,
-                            text: 'Join the Discord Server! https://discord.dragonruby.org',
-                            size_px: 30,
-                            anchor_x: 0.5 }
-
-  if args.inputs.keyboard.left
-    args.state.logo_rect.x -= 10
-  elsif args.inputs.keyboard.right
-    args.state.logo_rect.x += 10
+  # tick current scene
+  case current_scene
+  when :title_scene
+    tick_title_scene args
+  when :menu_scene
+    tick_menu_scene args
+  when :game_scene
+    tick_game_scene args
+  when :input_name_scene
+    tick_input_name_scene args
+  when :greeting_scene
+    tick_greeting_scene args
   end
 
-  if args.inputs.keyboard.down
-    args.state.logo_rect.y -= 10
-  elsif args.inputs.keyboard.up
-    args.state.logo_rect.y += 10
+  # ensure current_scene flag isn't set mid tick
+  if args.state.current_scene != current_scene
+    raise "Scene was changed incorrectly. Set args.state.next_scene to change scenes."
   end
 
-  if args.state.logo_rect.x > 1280
-    args.state.logo_rect.x = 0
-  elsif args.state.logo_rect.x < 0
-    args.state.logo_rect.x = 1280
+  # if next scene was set, transition current scene to next
+  if args.state.next_scene
+    args.state.current_scene = args.state.next_scene
+    args.state.next_scene = nil
+  end
+end
+
+def tick_title_scene args
+  args.outputs.labels << [640, 525, 'Welcome to', 5, 1]
+  args.outputs.labels << [640, 480, 'Siren Creek', 12, 1]
+  args.outputs.labels << [640, 100, 'click to start', 1, 1]
+  
+  if args.inputs.mouse.click
+    args.state.next_scene = :menu_scene
+  end
+end
+
+def tick_menu_scene args
+  args.outputs.labels << [640, 525, 'Main Menu', 5, 1]
+  args.outputs.labels << [640, 460, '[1] New Game', 1, 1]
+  args.outputs.labels << [640, 430, '[2] Back to Title', 1, 1]
+  
+  if args.inputs.keyboard.key_down.one
+    args.state.next_scene = :game_scene
   end
 
-  if args.state.logo_rect.y > 720
-    args.state.logo_rect.y = 0
-  elsif args.state.logo_rect.y < 0
-    args.state.logo_rect.y = 720
+  if args.inputs.keyboard.key_down.two
+    args.state.next_scene = :title_scene
+  end
+
+  if args.inputs.keyboard.key_down.escape
+    args.state.next_scene = :title_scene
+  end
+end
+
+def tick_game_scene args
+  args.outputs.labels << [640, 525, 'Welcome to', 5, 1]
+  args.outputs.labels << [640, 480, 'Siren Creek', 12, 1]
+  args.outputs.solids << {
+    x: 20,
+    y: 20,
+    w: 1240,
+    h: 100,
+    r: 0,
+    g: 0,
+    b: 0
+  }
+  args.outputs.labels << [40, 100, 'Oh! Hello there...', 1, 0, 255, 255, 255]
+  args.outputs.labels << [40, 70, 'You must have been traveling for a very long time to get here! What\'s your name?', 1, 0, 255, 255, 255]
+  args.outputs.labels << [1060, 55, '[click to continue]', 0, 0, 255, 255, 255]
+
+  if args.inputs.keyboard.key_down.escape
+    args.state.next_scene = :title_scene
+  end
+  if args.inputs.mouse.click
+    args.state.next_scene = :input_name_scene
+  end
+end
+
+def tick_input_name_scene args
+  # Create an input
+  args.state.input ||= Input::Text.new(x: 100, y: 600, w: 300, focussed: true)
+
+  # Allow the input to process inputs and render text (render_target)
+  args.state.input.tick
+
+  # Get the value
+  args.state.input_value = args.state.input.value
+
+  # Output the input
+  args.outputs.primitives << args.state.input
+
+  # Output the value
+  args.outputs.debug << { x: 100, y: 100, text: "Player Name: #{args.state.input_value}" }.label!
+
+  args.outputs.labels << [1005, 55, '[click enter to confirm]', 0, 0]
+
+  if args.inputs.keyboard.key_down.enter
+    args.state.player = args.state.input_value
+    args.state.next_scene = :greeting_scene
+  end
+
+  if args.inputs.keyboard.key_down.escape
+    args.state.next_scene = :title_scene
+  end
+end
+
+def tick_greeting_scene args
+  args.outputs.labels << [40, 100, "So your name is #{args.state.player}? Excellent!", 1, 0, 255, 255, 255]
+  args.outputs.solids << {
+    x: 20,
+    y: 20,
+    w: 1240,
+    h: 100,
+    r: 0,
+    g: 0,
+    b: 0
+  }
+
+  if args.inputs.keyboard.key_down.escape
+    args.state.next_scene = :title_scene
   end
 end
